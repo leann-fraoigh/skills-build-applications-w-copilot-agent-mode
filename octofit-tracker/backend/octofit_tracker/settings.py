e43fe6c5f19591207c8101fc3cdae460e376c190
Daria Myshlennikova <d.myshlennikova@gmail.com>
2025-10-16 12:32:35 +0000

    Настроить ALLOWED_HOSTS и обновить корневой API для поддержки CODESPACE_NAME

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = 'django-insecure--0&iel9$=yvj=s5i-n7raz(^o5g$x!iyns7&%@-#47@qw17aw%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
